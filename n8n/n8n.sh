e84e801328c7c994ae759967150066d252891a18
RiyadhBenhallou <riadhriadh2004@gmail.com>
2025-08-28 20:03:17 +0100

    Update dependencies for flowise and n8n; bump flowise to 3.0.5 and n8n to 1.106.3. Adjust development server port for flowise to 3030 and enable tunnel option for n8n. Update various package versions in package-lock.json for both projects.

--- n8n/n8n.sh
+++ n8n/n8n.sh
@@ -3,10 +3,11 @@
 cd dev/ai-automation/n8n && npm run dev &
 n8n_pid=$!
 
-ngrok http --url=chicken-apt-dinosaur.ngrok-free.app 5678 > /dev/null &
-ngrok_pid=$!
+# uncomment this command in case you wnated to use ngrok and remove the --tunnel flag from the scripts in package.json + add the ngrok pid alongside n8n pid in the kill command below
+# ngrok http --url=chicken-apt-dinosaur.ngrok-free.app 5678 > /dev/null &
+# ngrok_pid=$!
 
 # Wait for user to press Ctrl+C
-trap "echo 'Stopping...'; kill $n8n_pid $ngrok_pid; exit" SIGINT
+trap "echo 'Stopping...'; kill $n8n_pid; exit" SIGINT
 
 wait $n8n_pid
